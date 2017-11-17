503c60b13bfde0d20961c18aca19dbcc2102d4ca
C <c@Cs-iMac.local>
2017-11-17 14:23:27 +0300

    no message

---
+++ YMoji/Classes/YMojiCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  YMojiCollectionViewCell.swift
+//  YMoji
+//
+//  Created by C on 9/3/17.
+//  Copyright © 2017 C. All rights reserved.
+//
+
+import UIKit
+
+class YMojiCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var containerImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
